f46a57f9faa1662234c8160b7b6e936ce7db2709
Wagner Paulino <wagnersantos9778@gmail.com>
2020-12-14 20:30:02 -0300

    atualização do para o vue 3

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 import TodoStore from './todo/todo-store'
 import GithubRepositorieStore from './github-repositories/github-repositories-store'
 
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
+const store = createStore({
   modules: {
     todoStore: TodoStore,
     githubRepositorieStore: GithubRepositorieStore
